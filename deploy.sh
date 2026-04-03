662caadce4245925c54b0f2c9c9e8546cb1bae22
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2026-04-03 11:30:26 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,7 @@ mkdir -p ./docs/blog
 
 # TODO: Check that we have committed all of our files.
 
-nix build ./hakyll# && cp -r result/* ./docs/blog/
+# nix build ./hakyll# && cp -r result/* ./docs/blog/
+cd hakyll
+./result/bin/site rebuild
+cp -r _site/* ../docs/blog/
