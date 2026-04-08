436075c297d8cd89d88c1c8eff02543dcd9e767e
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2026-04-08 17:38:03 +0200

    `deploy.sh`: Stash and pop all changes

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+git stash --include-untracked
+
 mkdir -p ./docs/blog
 
 # TODO: Check that we have committed all of our files.
@@ -8,3 +10,5 @@ mkdir -p ./docs/blog
 cd hakyll
 ./result/bin/site rebuild
 cp -r _site/* ../docs/blog/
+
+git stash pop
