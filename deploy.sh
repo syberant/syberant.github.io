93b9a3b7a2cb36d5c967fced9f0bfd183511c10b
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2026-09-08 17:06:09 +0200

    Deploy website from git worktree
    Figured this should be possible and found
    https://leonscherer.com/blog/push-folder-to-separate-git-branch

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,12 @@
 set -euo pipefail
 
 # TODO: Check that we have committed all of our files.
-git stash --include-untracked
 
 cd "$(git rev-parse --show-toplevel)"
+nix-build
 
-# nix build ./hakyll#
-cd hakyll
-../result/bin/site rebuild
-
-git switch pages
-mkdir -p ./blog
-cp -r _site/* ../blog/
-
-cd ..
+rsync -rhv --delete ./result/* ./deployed/
+cd deployed
 git add --all
 git commit --message Deploy || printf "\x1b[93mFailed to commit, possibly because there were no changes\x1b[0m\n"
-
-git switch master
-git stash pop
-
+git push
